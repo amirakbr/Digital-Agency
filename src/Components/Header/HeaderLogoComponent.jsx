3fb1c48f93023f7271bb97490127dae3c5a4ab7a
amir.akbarzadeh <amir.akbrzadh3@gmail.com>
2022-11-24 22:30:56 +0330

    import header logo

---
+++ src/Components/Header/HeaderLogoComponent.jsx
@@ -0,0 +1,9 @@
+import { ReactComponent as HeaderLogo } from "./Assets/Svg/headerLogo.svg"
+
+
+let HeaderLogoComponent = () => {
+    return(
+        <HeaderLogo />
+    ) ; 
+}
+export default HeaderLogoComponent ; 
