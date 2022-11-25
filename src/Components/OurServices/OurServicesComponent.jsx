4a1d59a58444a188524fb214eebce5fcb8bc0980
amir.akbarzadeh <amir.akbrzadh3@gmail.com>
2022-11-25 23:41:53 +0330

    add our services section

--- src/Components/OurServices/OurServicesComponent.jsx
+++ src/Components/OurServices/OurServicesComponent.jsx
@@ -1,16 +1,41 @@
-import {ReactComponent as S} from '../../Assets/Svg/OurServicesSvg/WebDevelopment.svg'
-
-
-
 let OurServicesComponent = () => {
     return (
-        <div>
-            <h2>
+        <div className="container mx-auto px-[2rem]">
+            <h2 className="text-center text-xl text-white mt-[6.25rem] mb-[2.813rem] font-semibold">
                 Our Services
             </h2>
-            <div>
-                <div>
-                    <S />
+            <div className="grid grid-cols-[1fr_1fr_1fr_1fr] place-items-center text-white">
+                <div className="p-[1rem_3rem] bg-[#020616] flex flex-col items-center rounded-lg">
+                    <div className="h-[10rem] w-[11rem] flex items-center justify-center">
+                        <img src="./OurServices/UxResearch.png" alt="" />
+                    </div>
+                    <span className="font-semibold text-base mt-[1rem]">
+                        UxResearch
+                    </span>
+                </div>
+                <div className="p-[1rem_3rem] bg-[#020616] flex flex-col items-center rounded-lg">
+                    <div className="h-[10rem] w-[11rem] flex items-center justify-center">
+                        <img src="./OurServices/SpecialContent.png" alt="" />
+                    </div>
+                    <span className="font-semibold text-base mt-[1rem]">
+                        Special Content
+                    </span>
+                </div>
+                <div className="p-[1rem_3rem] bg-[#020616] flex flex-col items-center rounded-lg">
+                    <div className="h-[10rem] w-[11rem] flex items-center justify-center">
+                        <img src="./OurServices/WebDevelopment.png" alt="" />
+                    </div>
+                    <span className="font-semibold text-base mt-[1rem]">
+                        Web Development
+                    </span>
+                </div>
+                <div className="p-[1rem_3rem] bg-[#020616] flex flex-col items-center rounded-lg">
+                    <div className="h-[10rem] w-[11rem] flex items-center justify-center">
+                        <img src="./OurServices/Design.png" alt="" />
+                    </div>
+                    <span className="font-semibold text-base mt-[1rem]">
+                        Design
+                    </span>
                 </div>
             </div>
         </div>
