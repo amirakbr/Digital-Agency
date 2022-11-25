e8ec82bced8ed798fa37cc9051e69c91d6d6106f
amir.akbarzadeh <amir.akbrzadh3@gmail.com>
2022-11-25 19:31:43 +0330

    add text color and font weight to banner text

--- src/Components/FloatBanner/FloatBanner.jsx
+++ src/Components/FloatBanner/FloatBanner.jsx
@@ -2,7 +2,7 @@ let FloatBanner = () => {
     return (
         <div className="flex container mx-auto px-[2rem] mt-[4rem]">
             <div className="overflow-hidden text-2xl text-[#ffffff]">
-                <p className="whitespace-nowrap mr-[2rem]">
+                <p className="whitespace-nowrap mr-[2rem] text-[#6a6c77] font-semibold">
                     <span className="text-xl mr-[2rem]">                    
                         DESIGN * DEVELOP * PUBLISH
                     </span>
