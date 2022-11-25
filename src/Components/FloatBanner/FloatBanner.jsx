ac683a0f1c45acf471aa935588b1a98fa20cc633
amir.akbarzadeh <amir.akbrzadh3@gmail.com>
2022-11-25 19:28:01 +0330

    add float text banner

--- src/Components/FloatBanner/FloatBanner.jsx
+++ src/Components/FloatBanner/FloatBanner.jsx
@@ -3,22 +3,22 @@ let FloatBanner = () => {
         <div className="flex container mx-auto px-[2rem] mt-[4rem]">
             <div className="overflow-hidden text-2xl text-[#ffffff]">
                 <p className="whitespace-nowrap mr-[2rem]">
-                    <span>                    
+                    <span className="text-xl mr-[2rem]">                    
                         DESIGN * DEVELOP * PUBLISH
                     </span>
-                    <span>                    
+                    <span className="text-xl mr-[2rem]">                    
                         DESIGN * DEVELOP * PUBLISH
                     </span>
-                    <span>                    
+                    <span className="text-xl mr-[2rem]">                    
                         DESIGN * DEVELOP * PUBLISH
                     </span>
-                    <span>                    
+                    <span className="text-xl mr-[2rem]">                    
                         DESIGN * DEVELOP * PUBLISH
                     </span>
-                    <span>                    
+                    <span className="text-xl mr-[2rem]">                    
                         DESIGN * DEVELOP * PUBLISH
                     </span>
-                    <span>                    
+                    <span className="text-xl mr-[2rem]">                    
                         DESIGN * DEVELOP * PUBLISH
                     </span>
 
