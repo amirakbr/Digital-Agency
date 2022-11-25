ce75b6662bb070f1d9eeb24e92df6110c80222f7
amir.akbarzadeh <amir.akbrzadh3@gmail.com>
2022-11-25 19:27:08 +0330

    delete pre header logo component

--- src/Components/Header/HeaderLogoComponent.jsx
+++
@@ -1,9 +0,0 @@
-import { ReactComponent as HeaderLogo } from "../../Assets/Svg/headerLogo.svg"
-
-
-let HeaderLogoComponent = () => {
-    return(
-        <HeaderLogo className="w-[4.5rem]" />
-    ) ; 
-}
-export default HeaderLogoComponent ; 
