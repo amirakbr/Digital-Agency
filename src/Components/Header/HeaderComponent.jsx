35faf4a0f846057ed2615ea3d13f00c07687a74f
amir.akbarzadeh <amir.akbrzadh3@gmail.com>
2022-11-26 00:47:30 +0330

    add header animation and reafactor style's

--- src/Components/Header/HeaderComponent.jsx
+++ src/Components/Header/HeaderComponent.jsx
@@ -7,32 +7,48 @@ let HeaderComponent = () => {
                 <div className='flex'>
                     <HeaderLogo className="w-[4.5rem]" />
                     <div className='flex items-center ml-[2rem] text-lg'>
-                        <div className='text-center p-[.4rem_1.3rem]'>
-                            <p>
+                        <div className='text-center mr-[1.3rem] text-white transation-all duration-[.25s] hover:text-[#CDDA4F] after:content-["_"] after:w-full after:border-b-[.12rem] after:border-b-[solid] after:border-b-[orange] after:scale-x-[0] hover:after:scale-x-[1] after:transation-all after:duration-[.15s] after:origin-left cursor-pointer flex flex-col'>
+                            <p className="font-semibold p-[.4rem_.5rem]">
                                 Our Story
                             </p>
                         </div>
-                        <div className='text-center p-[.4rem_1.3rem]'>
-                            <p>
+                        <div className='projectLinks text-center mr-[1.3rem] text-white transation-all duration-[.25s] hover:text-[#CDDA4F] after:content-["_"] after:w-full after:border-b-[.12rem] after:border-b-[solid] after:border-b-[orange] after:scale-x-[0] hover:after:scale-x-[1] after:transation-all after:duration-[.15s] after:origin-left cursor-pointer flex flex-col'>
+                            <p className="font-semibold p-[.4rem_.5rem] flex items-baseline ">
                                 Projects
+                                <svg className="ml-[.5rem]"
+                                    width={14}
+                                    height={8}
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M7 5.172 2.05.222.636 1.636 7 8l6.364-6.364L11.95.222 7 5.172Z"
+                                        fill="#fff"
+                                    />
+                                </svg>
                             </p>
                         </div>
-                        <div className='text-center p-[.4rem_1.3rem]'>
-                            <p>
+                        <div className='text-center mr-[1.3rem] text-white transation-all duration-[.25s] hover:text-[#CDDA4F] after:content-["_"] after:w-full after:border-b-[.12rem] after:border-b-[solid] after:border-b-[orange] after:scale-x-[0] hover:after:scale-x-[1] after:transation-all after:duration-[.15s] after:origin-left cursor-pointer flex flex-col'>
+                            <p className="font-semibold p-[.4rem_.5rem]">
                                 Portfolio
                             </p>
                         </div>  
-                        <div className='text-center p-[.4rem_1.3rem]'>
-                            <p>
+                        <div className='text-center mr-[1.3rem] text-white transation-all duration-[.25s] hover:text-[#CDDA4F] after:content-["_"] after:w-full after:border-b-[.12rem] after:border-b-[solid] after:border-b-[orange] after:scale-x-[0] hover:after:scale-x-[1] after:transation-all after:duration-[.15s] after:origin-left cursor-pointer flex flex-col'>
+                            <p className="font-semibold p-[.4rem_.5rem]">
                                 Careers
                             </p>
                         </div>
                     </div>
                 </div>
                 <div className='p-[0rem_.8rem] text-lg'>
-                    <button className='p-[.7rem_1.2rem] bg-gradient-to-r from-[#FF8A52] to-[#CDDA4F] rounded text-black text-xl'>
-                        <p>
-                            Work with us
+                    <button className='p-[.5rem_1.2rem] bg-gradient-to-r from-[#FF8A52] to-[#CDDA4F] rounded text-black text-xl
+                     overflow-hidden w-[9.56rem] animate-backgroundsize bg-[length:200%_100%]'>
+                        <p className="flex whitespace-nowrap animate-wiggle">
+                            <span>
+                                Work with us
+                            </span>
+                            <span className="ml-[2rem]">
+                                Work with us
+                            </span>
                         </p>
                     </button>
                 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,23 @@ module.exports = {
     extend: {
       colors:{
         'headerbuttonbackgroundcolor' : 'linear-gradient(90deg, #FF8A52 0%, #CDDA4F 105.35%)'
+      },
+      keyframes: {
+        wiggle: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-9.2rem)' },
+        },
+        backgroundsize : {
+          '0%' : {backgroundPosition : '100% 0%'} , 
+          '50%' : {backgroundPosition : '0% 0%'} ,
+          '100%' : {backgroundPosition : '100% 0'} , 
+          //linear-gradient(90deg,#FF8A52,#CDDA4F,#FF8A52,#CDDA4F);
+        }
+      } , 
+      animation: {
+        wiggle: 'wiggle 10s linear infinite',
+        backgroundsize : 'backgroundsize 3s linear infinite'
       }
-
     },
   },
   plugins: [],
