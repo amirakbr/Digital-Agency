31a97cb3fc58519112fe60bcd3e8414198526255
amir.akbarzadeh <amir.akbrzadh3@gmail.com>
2022-11-26 15:26:42 +0330

    make our section responsive

--- src/Components/OurServices/OurServicesComponent.jsx
+++ src/Components/OurServices/OurServicesComponent.jsx
@@ -4,8 +4,8 @@ let OurServicesComponent = () => {
             <h2 className="text-center text-xl text-white mt-[6.25rem] mb-[2.813rem] font-semibold">
                 Our Services
             </h2>
-            <div className="grid grid-cols-[1fr_1fr_1fr_1fr] place-items-center text-white">
-                <div className="p-[1rem_3rem] bg-[#020616] flex flex-col items-center rounded-lg">
+            <div className="grid grid-cols-[1fr] place-content-center tablets:grid-cols-[1fr_1fr] laptops:grid-cols-[1fr_1fr_1fr] md:grid-cols-[1fr_1fr_1fr_1fr] place-items-center text-white">
+                <div className="p-[1rem_3rem] bg-[#020616] flex flex-col items-center rounded-lg mb-[2rem]">
                     <div className="h-[10rem] w-[11rem] flex items-center justify-center">
                         <img src="./OurServices/UxResearch.png" alt="" />
                     </div>
@@ -13,7 +13,7 @@ let OurServicesComponent = () => {
                         UxResearch
                     </span>
                 </div>
-                <div className="p-[1rem_3rem] bg-[#020616] flex flex-col items-center rounded-lg">
+                <div className="p-[1rem_3rem] bg-[#020616] flex flex-col items-center rounded-lg mb-[2rem]">
                     <div className="h-[10rem] w-[11rem] flex items-center justify-center">
                         <img src="./OurServices/SpecialContent.png" alt="" />
                     </div>
@@ -21,7 +21,7 @@ let OurServicesComponent = () => {
                         Special Content
                     </span>
                 </div>
-                <div className="p-[1rem_3rem] bg-[#020616] flex flex-col items-center rounded-lg">
+                <div className="p-[1rem_3rem] bg-[#020616] flex flex-col items-center rounded-lg mb-[2rem]">
                     <div className="h-[10rem] w-[11rem] flex items-center justify-center">
                         <img src="./OurServices/WebDevelopment.png" alt="" />
                     </div>
@@ -29,7 +29,7 @@ let OurServicesComponent = () => {
                         Web Development
                     </span>
                 </div>
-                <div className="p-[1rem_3rem] bg-[#020616] flex flex-col items-center rounded-lg">
+                <div className="p-[1rem_3rem] bg-[#020616] flex flex-col items-center rounded-lg mb-[2rem]  col-[unset] laptops:col-span-4 md:col-[unset]">
                     <div className="h-[10rem] w-[11rem] flex items-center justify-center">
                         <img src="./OurServices/Design.png" alt="" />
                     </div>
