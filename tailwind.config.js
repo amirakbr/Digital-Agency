4049b6a6840d7d59bfd44b37133253542c148420
amir.akbarzadeh <amir.akbrzadh3@gmail.com>
2022-11-26 15:25:38 +0330

    add animations and breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens : {
+      'phone' : '340px' , 
+      'tablets' : '768px' , 
+      'laptops' : '1100px' ,
+      'md' : '1280px' , 
+    } , 
     extend: {
       colors:{
         'headerbuttonbackgroundcolor' : 'linear-gradient(90deg, #FF8A52 0%, #CDDA4F 105.35%)'
@@ -17,13 +23,32 @@ module.exports = {
           '50%' : {backgroundPosition : '0% 0%'} ,
           '100%' : {backgroundPosition : '100% 0'} , 
           //linear-gradient(90deg,#FF8A52,#CDDA4F,#FF8A52,#CDDA4F);
+        } , 
+        hoverheadermenu : {
+          '0%' : {transform: 'translateY(2rem)'} , 
+          '100%' : {transform: 'translateY(0rem)'} , 
+        } , 
+        heroImageFloating : {
+          '0%' : {transform: 'translateY(-1.2rem)'} ,
+          '50%' : {transform: 'translateY(1.2rem)'} ,
+          '100%' : {transform: 'translateY(-1.2rem)'} , 
+        } , 
+        FloatBanner : {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-19rem)' },
         }
       } , 
       animation: {
         wiggle: 'wiggle 10s linear infinite',
-        backgroundsize : 'backgroundsize 3s linear infinite'
+        backgroundsize : 'backgroundsize 3s linear infinite' , 
+        heroImageFloating : 'heroImageFloating 10s linear infinite' , 
+        FloatBanner: 'FloatBanner 5s linear infinite',
+        hoverheadermenu: 'hoverheadermenu .2s linear forwards',
       }
     },
   },
   plugins: [],
 }
+/*
+translateX(-38.157rem)
+*/
