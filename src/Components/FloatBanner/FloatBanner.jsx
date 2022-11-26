cb4b1b7dfc7e94a97be67869ac18482e8431031b
amir.akbarzadeh <amir.akbrzadh3@gmail.com>
2022-11-26 15:21:34 +0330

    add float banner

--- src/Components/FloatBanner/FloatBanner.jsx
+++ src/Components/FloatBanner/FloatBanner.jsx
@@ -2,26 +2,25 @@ let FloatBanner = () => {
     return (
         <div className="flex container mx-auto px-[2rem] mt-[4rem]">
             <div className="overflow-hidden text-2xl text-[#ffffff]">
-                <p className="whitespace-nowrap mr-[2rem] text-[#6a6c77] font-semibold">
-                    <span className="text-xl mr-[2rem]">                    
+                <p className="whitespace-nowrap mr-[2rem] text-[#6a6c77] font-semibold animate-FloatBanner">
+                    <span className="text-xl mr-[2rem] w-[17rem] inline-block">                    
                         DESIGN * DEVELOP * PUBLISH
                     </span>
-                    <span className="text-xl mr-[2rem]">                    
+                    <span className="text-xl mr-[2rem] w-[17rem] inline-block">                    
                         DESIGN * DEVELOP * PUBLISH
                     </span>
-                    <span className="text-xl mr-[2rem]">                    
+                    <span className="text-xl mr-[2rem] w-[17rem] inline-block">                    
                         DESIGN * DEVELOP * PUBLISH
                     </span>
-                    <span className="text-xl mr-[2rem]">                    
+                    <span className="text-xl mr-[2rem] w-[17rem] inline-block">                    
                         DESIGN * DEVELOP * PUBLISH
                     </span>
-                    <span className="text-xl mr-[2rem]">                    
+                    <span className="text-xl mr-[2rem] w-[17rem] inline-block">                    
                         DESIGN * DEVELOP * PUBLISH
                     </span>
-                    <span className="text-xl mr-[2rem]">                    
+                    <span className="text-xl mr-[2rem] w-[17rem] inline-block">                    
                         DESIGN * DEVELOP * PUBLISH
                     </span>
-
                 </p>
             </div>
         </div>
