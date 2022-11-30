d1cb35217c1c50e4e08a2dccb3e9142862dd4f06
Amir <amir.akbrzadh3@gmail.com>
2022-12-01 02:27:56 +0330

    change button border color

--- src/Components/Hero/HeroComponent.jsx
+++ src/Components/Hero/HeroComponent.jsx
@@ -31,10 +31,10 @@ let HeroComponent = () => {
                 </p>
                 <button className='getStartedButton bg-[#102FD3] p-[.7rem_1.2rem] rounded mt-[3rem] text-lg font-semibold relative overflow-hidden'>
                     Get started
-                    <div className='getStartedButtonHover1 absolute w-[calc(100%+6px)] left-[-3px] top-0 shadow-[0_0_0_2.8px_#666c2b91]'></div>
-                    <div className='getStartedButtonHover2 absolute h-[calc(100%+6px)] top-[-3px] right-0 shadow-[0_0_0_2.8px_#666c2b91]'></div>
-                    <div className='getStartedButtonHover3 absolute w-[calc(100%+6px)] bottom-0 left-[-3px] shadow-[0_0_0_2.8px_#666c2b91]'></div>
-                    <div className='getStartedButtonHover4 absolute h-[calc(100%+6px)] top-[-3px] left-0 shadow-[0_0_0_2.8px_#666c2b91]'></div>
+                    <div className='getStartedButtonHover1 absolute w-[calc(100%+6px)] left-[-3px] top-0 shadow-[0_0_0_2.8px_#b2bb5d]'></div>
+                    <div className='getStartedButtonHover2 absolute h-[calc(100%+6px)] top-[-3px] right-0 shadow-[0_0_0_2.8px_#b2bb5d]'></div>
+                    <div className='getStartedButtonHover3 absolute w-[calc(100%+6px)] bottom-0 left-[-3px] shadow-[0_0_0_2.8px_#b2bb5d]'></div>
+                    <div className='getStartedButtonHover4 absolute h-[calc(100%+6px)] top-[-3px] left-0 shadow-[0_0_0_2.8px_#b2bb5d]'></div>
                 </button>
             </div>
             <div className='w-full p-[1rem_0.5rem] laptops:relative absolute laptops:left-0 laptops:top-0 left-[50%] top-[50%] laptops:max-w-[auto]
